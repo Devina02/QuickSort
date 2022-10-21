fd0ec7a2f37eaf4d6763442ad5bc6bb0b5f078f6
Devina02 <devina.fitriapngs@gmail.com>
2022-10-21 17:16:37 +0700

    membuat method input

--- Program.cs
+++ Program.cs
@@ -15,7 +15,30 @@ namespace QuickSort
 
         private int n;
 
-        
-        
+        void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements int the array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+            }
+            Console.WriteLine("\n===============================================");
+            Console.WriteLine(" Enter Array Elments");
+            Console.WriteLine("\n===============================================");
+
+            //get array elements
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.Write("<" + (i + i) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
     }
 }
