bc55092382e3aa2a1868a3288d38bdaccc9fa931
Devina02 <devina.fitriapngs@gmail.com>
2022-10-21 17:05:08 +0700

    Membuat class program

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace QuickSort
         //array of integers to hold values
         private int[] arr = new int[20];
         private int cmp_count = 0; // number of comparasion
-        private int mov_count = 0; //number of data movements
+        private int mov_count = 0; //number of data movements.
 
         private int n;
 
